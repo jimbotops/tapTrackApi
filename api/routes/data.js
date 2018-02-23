0fe1ec42cd99b4929b5a9020edb39f24d6342d0f
James XPS <jimbotops@hotmail.co.uk>
2018-02-23 12:41:25 +0000

    Added incrementation

--- api/routes/data.js
+++ api/routes/data.js
@@ -9,8 +9,7 @@ var Group = mongoose.model('Group');
 var User = mongoose.model('User');
 var app = express.Router();
 
-//TODO:update swagger for get and post
-
+// Sets a specific value using explicit target values
 app.post('/', function(req, res) {
   found = false;
   Group.findOne({title : req.decoded._doc.group}, function(err,group){
@@ -36,6 +35,36 @@ app.post('/', function(req, res) {
     }
   });
 });
+
+// Just increments the value using username from JWT - target should just be target, not target%%username
+app.post('/increment', function(req, res) {
+  found = false;
+  Group.findOne({title : req.decoded._doc.group}, function(err,group){
+    for (i=0; i<group.keyValue.length;i++){
+      console.log(req.decoded._doc.username);
+      console.log("total target: " + req.body.tar + '%%' + req.decoded._doc.username);
+      if (group.keyValue[i].target == req.body.tar + '%%' + req.decoded._doc.username){
+        found = true;
+         group.keyValue[i].value = group.keyValue[i].value+1;
+         group.save();
+         res.status(200).send({
+           id: 115,
+           updated : true
+         });
+         break;
+      }
+    }
+    if (!found){
+      group.keyValue.push({target: req.body.tar + '%%' + req.decoded._doc.username, value: 1})
+      group.save();
+      res.status(200).send({
+        id: 116,
+        created: true
+      });
+    }
+  });
+});
+
 
 app.get('/', function(req, res) {
   found = false;
